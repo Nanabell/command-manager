0635fbb6c6e9b061ae32b003c3d5a7ef11c911fa
Nanabell <marlon.delfin@gmail.com>
2021-11-05 07:53:33 +0100

    Micronaut Provider - Require ApplicationContext explicitly now

--- micronaut-provider/src/main/kotlin/dev/nanabell/jda/command/manager/provider/CommandManagerBuilderExtension.kt
+++ micronaut-provider/src/main/kotlin/dev/nanabell/jda/command/manager/provider/CommandManagerBuilderExtension.kt
@@ -1,24 +1,11 @@
 package dev.nanabell.jda.command.manager.provider
 
 import dev.nanabell.jda.command.manager.CommandManagerBuilder
+import io.micronaut.context.ApplicationContext
 import io.micronaut.context.annotation.Context
 
-
-@Context
-class CommandManagerBuilderExtension(provider: MicronautCommandProvider) {
-
-    init {
-        CommandManagerBuilderExtension.provider = provider
-    }
-
-    companion object {
-        internal lateinit var provider: MicronautCommandProvider
-    }
-}
-
-
 @Suppress("unused")
-fun CommandManagerBuilder.setMicronautProvider(): CommandManagerBuilder {
-    this.setCommandProvider(CommandManagerBuilderExtension.provider)
+fun CommandManagerBuilder.setMicronautProvider(context: ApplicationContext): CommandManagerBuilder {
+    this.setCommandProvider(MicronautCommandProvider(context))
     return this
 }
